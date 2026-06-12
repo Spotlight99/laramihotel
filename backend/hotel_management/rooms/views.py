782b0b786411cfe12df7344ef424c243c183949a
Your Name <your-email@example.com>
2026-06-12 16:28:36 +0100

    working on auth

--- backend/hotel_management/rooms/views.py
+++ backend/hotel_management/rooms/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, status, filters
-from rest_framework.decorators import action
+from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from datetime import datetime, timedelta
@@ -9,6 +9,7 @@ from .serializers import RoomSerializer, RoomDetailSerializer, HotelSerializer
 from rest_framework.decorators import api_view
 
 @api_view(["GET"])
+@permission_classes([AllowAny])
 def debug_rooms(request):
     return Response({
         "rooms": Room.objects.count(),
