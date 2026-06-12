7dbb3e0b87d40d668699ff2cfeac31ffc57d4849
Your Name <your-email@example.com>
2026-06-12 17:46:59 +0100

    Added basenames

--- backend/hotel_management/bookings/urls.py
+++ backend/hotel_management/bookings/urls.py
@@ -3,9 +3,24 @@ from rest_framework.routers import DefaultRouter
 from .views import RoomBookingViewSet, InvoiceViewSet, HouseKeepingViewSet
 
 router = DefaultRouter()
-router.register(r'', RoomBookingViewSet, basename='booking')
-router.register(r'invoices', InvoiceViewSet)
-router.register(r'housekeeping', HouseKeepingViewSet)
+
+router.register(
+    r'',
+    RoomBookingViewSet,
+    basename='booking'
+)
+
+router.register(
+    r'invoices',
+    InvoiceViewSet,
+    basename='invoice'
+)
+
+router.register(
+    r'housekeeping',
+    HouseKeepingViewSet,
+    basename='housekeeping'
+)
 
 urlpatterns = [
     path('', include(router.urls)),
